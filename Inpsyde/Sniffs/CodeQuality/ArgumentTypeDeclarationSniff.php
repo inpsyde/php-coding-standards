5169ab80b2b060edb58d2a6f110b698aba1da39f
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2018-02-05 13:17:28 +0100

    Replace NeutronStandard TypeHint with custom sniffs

---
+++ Inpsyde/Sniffs/CodeQuality/ArgumentTypeDeclarationSniff.php
@@ -0,0 +1,123 @@
+<?php declare(strict_types=1); # -*- coding: utf-8 -*-
+/*
+ * This file is part of the php-coding-standards package.
+ *
+ * (c) Inpsyde GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file contains code from "phpcs-neutron-standard" repository
+ * found at https://github.com/Automattic/phpcs-neutron-standard
+ * Copyright (c) Automattic
+ * released under MIT license.
+ */
+
+namespace Inpsyde\InpsydeCodingStandard\Sniffs\CodeQuality;
+
+use Inpsyde\InpsydeCodingStandard\Helpers;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class ArgumentTypeDeclarationSniff implements Sniff
+{
+    const TYPE_CODES = [
+        T_STRING,
+        T_ARRAY_HINT,
+        T_CALLABLE,
+        T_SELF,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_FUNCTION, T_CLOSURE];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(File $file, $position)
+    {
+        $tokens = $file->getTokens();
+
+        $paramsStart = $tokens[$position]['parenthesis_opener'] ?? 0;
+        $paramsEnd = $tokens[$position]['parenthesis_closer'] ?? 0;
+
+        if (!$paramsStart || !$paramsEnd || $paramsStart >= ($paramsEnd - 1)) {
+            return;
+        }
+
+        $variables = Helpers::filterTokensByType($paramsStart, $paramsEnd, $file, T_VARIABLE);
+
+        foreach (array_keys($variables) as $varPosition) {
+            $typePosition = $file->findPrevious(
+                [T_WHITESPACE, T_ELLIPSIS],
+                $varPosition - 1,
+                $paramsStart + 1,
+                true
+            );
+
+            $type = $tokens[$typePosition] ?? null;
+            if ($type && in_array($type['code'], self::TYPE_CODES, true)) {
+                continue;
+            }
+
+            if (!Helpers::isHookClosure($file, $position)) {
+                $file->addWarning('Argument type is missing', $position, 'NoArgumentType');
+            }
+        }
+    }
+
+    /**
+     * @param File $file
+     * @param int $position
+     * @param array $tokens
+     * @return bool
+     */
+    private function isAddHook(File $file, int $position, array $tokens): bool
+    {
+        $lookForComma = $file->findPrevious(
+            [T_WHITESPACE],
+            $position - 1,
+            null,
+            true,
+            null,
+            true
+        );
+
+        if (!$lookForComma || ($tokens[$lookForComma]['code'] ?? '') !== T_COMMA) {
+            return false;
+        }
+
+        $functionCallOpen = $file->findPrevious(
+            [T_OPEN_PARENTHESIS],
+            $lookForComma - 2,
+            null,
+            false,
+            null,
+            true
+        );
+
+        if (!$functionCallOpen) {
+            return false;
+        }
+
+        $functionCall = $file->findPrevious(
+            [T_WHITESPACE],
+            $functionCallOpen - 1,
+            null,
+            true,
+            null,
+            true
+        );
+
+        return in_array(
+            ($tokens[$functionCall]['content'] ?? ''),
+            ['add_action', 'add_filter'],
+            true
+        );
+    }
+}
---
+++ Inpsyde/Sniffs/CodeQuality/ReturnTypeDeclarationSniff.php
@@ -0,0 +1,132 @@
+<?php declare(strict_types=1); # -*- coding: utf-8 -*-
+/*
+ * This file is part of the php-coding-standards package.
+ *
+ * (c) Inpsyde GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file contains code from "phpcs-neutron-standard" repository
+ * found at https://github.com/Automattic/phpcs-neutron-standard
+ * Copyright (c) Automattic
+ * released under MIT license.
+ */
+
+namespace Inpsyde\InpsydeCodingStandard\Sniffs\CodeQuality;
+
+use Inpsyde\InpsydeCodingStandard\Helpers;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class ReturnTypeDeclarationSniff implements Sniff
+{
+    const TYPE_CODES = [
+        T_STRING,
+        T_ARRAY_HINT,
+        T_CALLABLE,
+        T_SELF,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_FUNCTION, T_CLOSURE];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(File $file, $position)
+    {
+        list($functionStart, $functionEnd) = Helpers::functionBoundaries($file, $position);
+        if (!$functionStart < 0 || $functionEnd <= 0) {
+            return;
+        }
+
+        list($hasNonVoidReturnType, $hasVoidReturnType, $hasNoReturnType) = $this->returnTypeInfo(
+            $file,
+            $position,
+            $functionEnd
+        );
+
+        list($nonVoidReturnCount, $voidReturnCount) = Helpers::countReturns($file, $position);
+
+        $this->maybeErrors(
+            $hasNonVoidReturnType,
+            $hasVoidReturnType,
+            $hasNoReturnType,
+            $nonVoidReturnCount,
+            $voidReturnCount,
+            $file,
+            $position
+        );
+    }
+
+    /**
+     * @param bool $hasNonVoidReturnType
+     * @param bool $hasVoidReturnType
+     * @param bool $hasNoReturnType
+     * @param int $nonVoidReturnCount
+     * @param int $voidReturnCount
+     * @param File $file
+     * @param int $position
+     */
+    private function maybeErrors(
+        bool $hasNonVoidReturnType,
+        bool $hasVoidReturnType,
+        bool $hasNoReturnType,
+        int $nonVoidReturnCount,
+        int $voidReturnCount,
+        File $file,
+        int $position
+    ) {
+        if ($hasNonVoidReturnType && ($nonVoidReturnCount === 0 || $voidReturnCount > 0)) {
+            $msg = 'Return type with';
+            $file->addError(
+                $nonVoidReturnCount === 0 ? "{$msg} no return" : "{$msg} void return",
+                $position,
+                $nonVoidReturnCount === 0 ? 'MissingReturn' : 'IncorrectVoidReturn'
+            );
+        }
+
+        if ($nonVoidReturnCount <= 0) {
+            return;
+        }
+
+        if ($hasNoReturnType) {
+            $file->addWarning('Return type is missing', $position, 'NoReturnType');
+        }
+
+        if ($hasVoidReturnType) {
+            $file->addError(
+                'Void return type when returning non-void',
+                $position,
+                'IncorrectVoidReturnType'
+            );
+        }
+    }
+
+    /**
+     * @param File $file
+     * @param int $functionPosition
+     * @param int $functionEnd
+     * @return array
+     */
+    private function returnTypeInfo(File $file, int $functionPosition, int $functionEnd): array
+    {
+        $returnType = $file->findNext(
+            T_RETURN_TYPE,
+            $functionPosition + 3, // open parenthesis, close parenthesis, colon
+            $functionEnd - 1
+        );
+
+        $hasNonVoidReturnType = $returnType && $returnType['content'] !== 'void';
+        $hasVoidReturnType = $returnType && $returnType['content'] === 'void';
+        $hasNoReturnType = !$returnType;
+
+        return [$hasNonVoidReturnType, $hasVoidReturnType, $hasNoReturnType];
+    }
+}
