d74cb03a2185f073c635bf59b1feca2778ff6edb
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2018-02-12 17:20:23 +0100

    Rename NoSetterSniff to NoAccessorsSniff
    and warn for getters as well.

---
+++ Inpsyde/Sniffs/CodeQuality/NoAccessorsSniff.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1); # -*- coding: utf-8 -*-
+/*
+ * This file is part of the php-coding-standards package.
+ *
+ * (c) Inpsyde GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file contains code from "phpcs-calisthenics-rules" repository
+ * found at https://github.com/object-calisthenics
+ * Copyright (c) 2014 Doctrine Project
+ * released under MIT license.
+ */
+
+namespace Inpsyde\InpsydeCodingStandard\Sniffs\CodeQuality;
+
+use Inpsyde\InpsydeCodingStandard\Helpers;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * @package php-coding-standards
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+final class NoAccessorsSniff implements Sniff
+{
+    public $allowSetUp = true;
+
+    public $skipForFunctions = true;
+
+    /**
+     * @return int[]
+     */
+    public function register()
+    {
+        return [T_FUNCTION];
+    }
+
+    /**
+     * @param File $file
+     * @param int $position
+     * @throws \PHP_CodeSniffer\Exceptions\RuntimeException
+     */
+    public function process(File $file, $position)
+    {
+        if ($this->skipForFunctions && !Helpers::functionIsMethod($file, $position)) {
+            return;
+        }
+
+        $functionName = $file->getDeclarationName($position);
+
+        if (!$functionName || ($functionName === 'setUp' && $this->allowSetUp)) {
+            return;
+        }
+
+        preg_match('/^(set|get)[a-zA-Z0-9_\x7f-\xff]+/', $functionName, $matches);
+        if (!$matches) {
+            return;
+        }
+
+        if ($matches[1] === 'set') {
+            $file->addWarning(
+                'Setters are discouraged. Try to use immutable objects, constructor injection '
+                . 'and for objects that really needs changing state try behavior naming instead, '
+                . 'e.g. changeName() instead of setName().',
+                $position,
+                'NoSetter'
+            );
+        }
+
+        $file->addWarning(
+            'Getters are discouraged. "Tell Don\'t Ask" principle should be applied if possible, '
+            . 'and if getters are really needed consider naming methods after properties, '
+            . 'e.g. name() instead of getName().',
+            $position,
+            'NoGetter'
+        );
+    }
+}
